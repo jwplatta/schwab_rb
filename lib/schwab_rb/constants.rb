fabbfc45dbc7a97fb1d48a2022f93982616e62c2
Joseph Platta <jwplatta@gmail.com>
2024-12-03 00:19:18 -0600

    wrap constants in module

--- lib/schwab_rb/constants.rb
+++ lib/schwab_rb/constants.rb
@@ -1,2 +1,4 @@
-SCHWAB_BASE_URL="https://api.schwabapi.com"
-TOKEN_ENDPOINT="https://api.schwabapi.com/v1/oauth/token"
+module SchwabRb::Constants
+  SCHWAB_BASE_URL="https://api.schwabapi.com"
+  TOKEN_ENDPOINT="https://api.schwabapi.com/v1/oauth/token"
+end
