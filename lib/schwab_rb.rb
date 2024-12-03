8f63953668814eaa0b790589fa1857dbeb9a65ef
Joseph Platta <jwplatta@gmail.com>
2024-12-03 00:25:01 -0600

    requires for token manager, from login flow, from token file, auth context, client login_flow_server, constants

--- lib/schwab_rb.rb
+++ lib/schwab_rb.rb
@@ -2,8 +2,14 @@
 
 require_relative "schwab_rb/version"
 require_relative "schwab_rb/schwab"
+require_relative "schwab_rb/auth/token_manager"
+require_relative "schwab_rb/auth/from_login_flow"
+require_relative "schwab_rb/auth/from_token_file"
+require_relative "schwab_rb/auth/auth_context"
+require_relative "schwab_rb/clients/client"
+require_relative "schwab_rb/auth/login_flow_server"
+require_relative "schwab_rb/constants"
 
 module SchwabRb
   class Error < StandardError; end
-  # Your code goes here...
 end
