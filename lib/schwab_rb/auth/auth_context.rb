f5ce9f7be0361a5e007ae2b33e7aca474a82262d
Joseph Platta <jwplatta@gmail.com>
2024-12-03 21:07:18 -0600

    turn the auth context struct into a class and add a class method to build an instance

--- lib/schwab_rb/auth/auth_context.rb
+++ lib/schwab_rb/auth/auth_context.rb
@@ -1,5 +1,23 @@
+require 'oauth2'
+
 module SchwabRb::Auth
-  AuthContext = Struct.new(
-    :api_key, :callback_url, :authorization_url, :state
-  )
+  class AuthContext
+    class << self
+      def build(oauth_client, callback_url, authorization_url, state: nil)
+        auth_params = { redirect_uri: callback_url }
+        auth_params[:state] = state if state
+        authorization_url = oauth_client.auth_code.authorize_url(auth_params)
+
+        new(callback_url, authorization_url, state)
+      end
+    end
+
+    def initialize(callback_url, authorization_url, state)
+      @callback_url = callback_url
+      @authorization_url = authorization_url
+      @state = state
+    end
+
+    attr_reader :callback_url, :authorization_url, :state
+  end
 end
