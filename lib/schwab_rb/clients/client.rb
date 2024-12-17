48ea46d278e0d9ec981b56052758690704772582
Joseph Platta <jwplatta@gmail.com>
2024-12-16 23:58:49 -0600

    move init up to base client

--- lib/schwab_rb/clients/client.rb
+++ lib/schwab_rb/clients/client.rb
@@ -2,15 +2,6 @@ require_relative 'base_client'
 
 module SchwabRb
   class Client < BaseClient
-    attr_reader :api_key, :session, :token_metadata, :enforce_enums
-
-    def initialize(api_key, session, token_metadata:, enforce_enums: true)
-      @api_key = api_key
-      @session = session
-      @token_metadata = token_metadata
-      @enforce_enums = enforce_enums
-    end
-
     private
 
     def get(path, params)
