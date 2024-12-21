c0516a4031624856e841f3889c276c71f5f24700
Joseph Platta <jwplatta@gmail.com>
2024-12-21 13:07:46 -0600

    add class method to return projection constants

--- lib/schwab_rb/instrument.rb
+++ lib/schwab_rb/instrument.rb
@@ -1,12 +1,16 @@
 module SchwabRb
   class Instrument
-    module Projections
+    module Projection
       SYMBOL_SEARCH = 'symbol-search'
       SYMBOL_REGEX = 'symbol-regex'
       DESCRIPTION_SEARCH = 'desc-search'
       DESCRIPTION_REGEX = 'desc-regex'
       SEARCH = 'search'
       FUNDAMENTAL = 'fundamental'
+    end
+
+    def self.projections
+      Projection.constants.map { |const| Projection.const_get(const) }
     end
   end
 end
