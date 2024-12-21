531528e432b2447f9acdd589f84113be94d501cf
Joseph Platta <jwplatta@gmail.com>
2024-12-21 13:51:07 -0600

    add specs for correct order statuses and types

--- spec/orders/order_spec.rb
+++ spec/orders/order_spec.rb
@@ -4,4 +4,14 @@ describe SchwabRb::Order do
   it "does not raise" do
     expect { described_class.new }.not_to raise_error
   end
+  describe 'constants' do
+    it 'returns correct statuses' do
+      statuses = SchwabRb::Order::Status.constants.map { |const| SchwabRb::Order::Status.const_get(const) }
+      expect(described_class.statuses).to match_array(statuses)
+    end
+    it 'returns correct types' do
+      types = SchwabRb::Order::Type.constants.map { |const| SchwabRb::Order::Type.const_get(const) }
+      expect(described_class.types).to match_array(types)
+    end
+  end
 end
