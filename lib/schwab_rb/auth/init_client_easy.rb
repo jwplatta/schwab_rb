caccf7a6de65b05f5f99a408aa4278a6e0669a60
Joseph Platta <jwplatta@gmail.com>
2024-12-23 23:12:00 -0600

    implement init client easy that wraps init client from token file and then fallsback to init client with the login flow

---
+++ lib/schwab_rb/auth/init_client_easy.rb
@@ -0,0 +1,41 @@
+require 'oauth2'
+require_relative 'init_client_token_file'
+require_relative 'init_client_login'
+
+module SchwabRb::Auth
+  def self.init_client_easy(
+    api_key,
+    app_secret,
+    callback_url,
+    token_path,
+    asyncio: false,
+    enforce_enums: false,
+    callback_timeout: 300.0,
+    interactive: true,
+    requested_browser: nil)
+
+    begin
+      client = SchwabRb::Auth::init_client_token_file(
+        api_key,
+        app_secret,
+        token_path,
+        enforce_enums: enforce_enums
+      )
+      raise OAuth2::Error.new("Token expired") if client.session.expired?
+    rescue
+      client = SchwabRb::Auth::init_client_login(
+        api_key,
+        app_secret,
+        callback_url,
+        token_path,
+        asyncio: asyncio,
+        enforce_enums: enforce_enums,
+        callback_timeout: callback_timeout,
+        interactive: interactive,
+        requested_browser: requested_browser
+      )
+    end
+
+    client
+  end
+end
