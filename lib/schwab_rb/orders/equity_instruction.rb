217a65ef7f514edc94d5f155a89d794d88d480c6
Joseph Platta <jwplatta@gmail.com>
2024-12-25 22:49:49 -0600

    add equity instruction constants to orders module

---
+++ lib/schwab_rb/orders/equity_instruction.rb
@@ -0,0 +1,11 @@
+module SchwabRb
+  module Orders
+    module EquityInstruction
+      # Instructions for opening and closing equity positions.
+      BUY = 'BUY'
+      SELL = 'SELL'
+      SELL_SHORT = 'SELL_SHORT'
+      BUY_TO_COVER = 'BUY_TO_COVER'
+    end
+  end
+end
