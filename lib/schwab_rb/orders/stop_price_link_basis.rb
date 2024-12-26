842f3d3737ac147f444a9f7e23419c6e2ae34b5d
Joseph Platta <jwplatta@gmail.com>
2024-12-25 22:51:22 -0600

    add stop price link basis constants to orders module

---
+++ lib/schwab_rb/orders/stop_price_link_basis.rb
@@ -0,0 +1,15 @@
+module SchwabRb
+  module Orders
+    module StopPriceLinkBasis
+      MANUAL = 'MANUAL'
+      BASE = 'BASE'
+      TRIGGER = 'TRIGGER'
+      LAST = 'LAST'
+      BID = 'BID'
+      ASK = 'ASK'
+      ASK_BID = 'ASK_BID'
+      MARK = 'MARK'
+      AVERAGE = 'AVERAGE'
+    end
+  end
+end
