c702066126a8e0f09878ef281f78eef73b8c3668
Joseph Platta <jwplatta@gmail.com>
2024-12-25 22:50:05 -0600

    add options instruction constants to orders module

---
+++ lib/schwab_rb/orders/option_instruction.rb
@@ -0,0 +1,11 @@
+module SchwabRb
+  module Orders
+    module OptionInstruction
+      # Instructions for opening and closing options positions.
+      BUY_TO_OPEN = 'BUY_TO_OPEN'
+      SELL_TO_CLOSE = 'SELL_TO_CLOSE'
+      SELL_TO_OPEN = 'SELL_TO_OPEN'
+      BUY_TO_CLOSE = 'BUY_TO_CLOSE'
+    end
+  end
+end
