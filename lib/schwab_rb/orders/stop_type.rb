b7ee68ba644e7a2d50791e34b957edfd42c3c436
Joseph Platta <jwplatta@gmail.com>
2024-12-25 22:51:45 -0600

    add stop type constants to orders module

---
+++ lib/schwab_rb/orders/stop_type.rb
@@ -0,0 +1,11 @@
+module SchwabRb
+  module Orders
+    module StopType
+      STANDARD = 'STANDARD'
+      BID = 'BID'
+      ASK = 'ASK'
+      LAST = 'LAST'
+      MARK = 'MARK'
+    end
+  end
+end
