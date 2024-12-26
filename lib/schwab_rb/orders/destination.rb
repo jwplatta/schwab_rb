9bd34d9d69c8f82d002b0e9942ac36e6c5888220
Joseph Platta <jwplatta@gmail.com>
2024-12-25 22:49:36 -0600

    add destination constants to orders module

---
+++ lib/schwab_rb/orders/destination.rb
@@ -0,0 +1,19 @@
+module SchwabRb
+  module Orders
+    module Destination
+      # Destinations for when you want to request a specific destination for your order.
+      INET = 'INET'
+      ECN_ARCA = 'ECN_ARCA'
+      CBOE = 'CBOE'
+      AMEX = 'AMEX'
+      PHLX = 'PHLX'
+      ISE = 'ISE'
+      BOX = 'BOX'
+      NYSE = 'NYSE'
+      NASDAQ = 'NASDAQ'
+      BATS = 'BATS'
+      C2 = 'C2'
+      AUTO = 'AUTO'
+    end
+  end
+end
