41d5a9edd6b0b5f5383cf86bf1941739d3c782a3
Joseph Platta <jwplatta@gmail.com>
2024-12-25 22:51:58 -0600

    add tak lot method constants to orders module

---
+++ lib/schwab_rb/orders/tax_lot_method.rb
@@ -0,0 +1,13 @@
+module SchwabRb
+  module Orders
+    module TaxLotMethod
+      FIFO = 'FIFO'
+      LIFO = 'LIFO'
+      HIGH_COST = 'HIGH_COST'
+      LOW_COST = 'LOW_COST'
+      AVERAGE_COST = 'AVERAGE_COST'
+      SPECIFIC_LOT = 'SPECIFIC_LOT'
+      LOSS_HARVESTER = 'LOSS_HARVESTER'
+    end
+  end
+end
