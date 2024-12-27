7f4690ec25b4627b3777d5b92eadb7a3109fb4f7
Joseph Platta <jwplatta@gmail.com>
2024-12-26 22:54:42 -0600

    use helper method to get the valid enum values from the module

--- lib/schwab_rb/utils/enum_enforcer.rb
+++ lib/schwab_rb/utils/enum_enforcer.rb
@@ -27,31 +27,35 @@ module EnumEnforcer
     raise ArgumentError, "expected type \"#{required_enum_type}\", got type \"#{value.class}\". #{possible_members_message}(initialize with enforce_enums: false to disable this checking)"
   end
 
-  def convert_enum(value, valid_enum_types)
+  def convert_enum(value, enum_type)
     return nil if value.nil?
 
-    if valid_enum_types.include? value
+    valid_values = enum_type.constants.map { |const| enum_type.const_get(const) }
+
+    if valid_values.include? value
       value
     elsif enforce_enums
-      type_error(value, required_enum_type)
+      type_error(value, enum_type)
     else
       value
     end
   end
 
-  def convert_enum_iterable(iterable, valid_enum_types)
+  def convert_enum_iterable(iterable, enum_type)
     return nil if iterable.nil?
 
-    if valid_enum_types.include? iterable
+    valid_values = get_valid_enum_values(enum_type)
+
+    if valid_values.include? iterable
       return [iterable]
     end
 
     values = []
     iterable.each do |value|
-      if valid_enum_types.include? value
+      if valid_values.include? value
         values << value
       elsif enforce_enums
-        type_error(value, required_enum_type)
+        type_error(value, enum_type)
       else
         values << value
       end
@@ -62,5 +66,9 @@ module EnumEnforcer
 
   def set_enforce_enums(enforce_enums)
     @enforce_enums = enforce_enums
+  end
+
+  def get_valid_enum_values(enum_type)
+    enum_type.constants.map { |const| enum_type.const_get(const) }
   end
 end
