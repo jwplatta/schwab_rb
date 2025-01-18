ee7f8d7e839896efa743923affe11306d7d0e0a0
Joseph Platta <jwplatta@gmail.com>
2025-01-17 21:37:42 -0600

    format client

--- lib/schwab_rb/clients/client.rb
+++ lib/schwab_rb/clients/client.rb
@@ -1,11 +1,13 @@
-require 'json'
-require 'net/http'
-require 'uri'
-require_relative 'base_client'
+# frozen_string_literal: true
+
+require "json"
+require "net/http"
+require "uri"
+require_relative "base_client"
 
 module SchwabRb
   class Client < BaseClient
-    BASE_URL = 'https://api.schwabapi.com'.freeze
+    BASE_URL = "https://api.schwabapi.com"
 
     private
 
@@ -14,7 +16,7 @@ module SchwabRb
       dest.query = URI.encode_www_form(params) if params.any?
 
       req_num = req_num()
-      log_request('GET', req_num, dest, params)
+      log_request("GET", req_num, dest, params)
       response = session.get(dest)
 
       log_response(response, req_num)
@@ -26,9 +28,9 @@ module SchwabRb
       dest = URI("#{BASE_URL}#{path}")
 
       req_num = req_num()
-      log_request('POST', req_num, dest, data)
+      log_request("POST", req_num, dest, data)
 
-      response = session.post(dest, data.to_json, { 'Content-Type' => 'application/json' })
+      response = session.post(dest, data.to_json, { "Content-Type" => "application/json" })
       log_response(response, req_num)
       register_redactions_from_response(response)
       response
@@ -38,9 +40,9 @@ module SchwabRb
       dest = URI("#{BASE_URL}#{path}")
 
       req_num = req_num()
-      log_request('PUT', req_num, dest, data)
+      log_request("PUT", req_num, dest, data)
 
-      response = session.put(dest, data.to_json, { 'Content-Type' => 'application/json' })
+      response = session.put(dest, data.to_json, { "Content-Type" => "application/json" })
       log_response(response, req_num)
       register_redactions_from_response(response)
       response
@@ -50,7 +52,7 @@ module SchwabRb
       dest = URI("#{BASE_URL}#{path}")
 
       req_num = req_num()
-      log_request('DELETE', req_num, dest)
+      log_request("DELETE", req_num, dest)
 
       response = session.delete(dest)
       log_response(response, req_num)
@@ -58,7 +60,6 @@ module SchwabRb
       response
     end
 
-    # Private helper methods
     def log_request(method, req_num, dest, data = nil)
       puts "Req #{req_num}: #{method} to #{dest}"
       puts "Payload: #{JSON.pretty_generate(data)}" if data
