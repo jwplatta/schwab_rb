6784544a829f4dec464072a9ca8c72b3f78ae145
Joseph Platta <jwplatta@gmail.com>
2025-01-17 21:37:31 -0600

    require mock transaction responses

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,11 +6,11 @@ require "pry"
 require "dotenv"
 require_relative "./factories/accounts"
 require_relative "./factories/orders"
+require_relative "./factories/transactions"
 
 Dotenv.load
 
 RSpec.configure do |config|
   config.example_status_persistence_file_path = ".rspec_status"
   config.filter_run_when_matching :focus
-  config.filter_run :focus
 end
