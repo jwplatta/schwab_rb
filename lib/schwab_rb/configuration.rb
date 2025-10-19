68f307b9bd0bfdd8b5ef3aaf3f87d0b619b6693e
Joseph Platta <jwplatta@gmail.com>
2025-10-19 13:10:59 -0500

    configure account management paths

--- lib/schwab_rb/configuration.rb
+++ lib/schwab_rb/configuration.rb
@@ -2,13 +2,19 @@
 
 module SchwabRb
   class Configuration
-    attr_accessor :logger, :log_file, :log_level, :silence_output
+    attr_accessor :logger, :log_file, :log_level, :silence_output,
+                  :schwab_home, :account_hashes_path, :account_names_path
 
     def initialize
       @logger = nil
       @log_file = ENV.fetch("SCHWAB_LOGFILE", nil)
       @log_level = ENV.fetch("SCHWAB_LOG_LEVEL", "WARN").upcase
       @silence_output = ENV.fetch("SCHWAB_SILENCE_OUTPUT", "false").downcase == "true"
+
+      default_home = File.expand_path("~/.schwab_rb")
+      @schwab_home = ENV.fetch("SCHWAB_HOME", default_home)
+      @account_hashes_path = ENV.fetch("SCHWAB_ACCOUNT_HASHES_PATH", File.join(@schwab_home, "account_hashes.json"))
+      @account_names_path = ENV.fetch("SCHWAB_ACCOUNT_NAMES_PATH", File.join(@schwab_home, "account_names.json"))
     end
 
     def has_external_logger?
