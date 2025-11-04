d38c2d965a3599812e68ed54430ca692896be6e5
Joseph Platta <jwplatta@gmail.com>
2025-11-03 21:11:50 -0600

    add test for vertical roll order

--- spec/orders/order_factory_spec.rb
+++ spec/orders/order_factory_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe SchwabRb::Orders::OrderFactory do
       # Set up mocks for all order classes before any tests run
       allow(SchwabRb::Orders::IronCondorOrder).to receive(:build).and_return(order_builder)
       allow(SchwabRb::Orders::VerticalOrder).to receive(:build).and_return(order_builder)
+      allow(SchwabRb::Orders::VerticalRollOrder).to receive(:build).and_return(order_builder)
       allow(SchwabRb::Orders::SingleOrder).to receive(:build).and_return(order_builder)
       allow(SchwabRb::Orders::OcoOrder).to receive(:build).and_return(order_builder)
     end
@@ -251,6 +252,72 @@ RSpec.describe SchwabRb::Orders::OrderFactory do
           duration: SchwabRb::Orders::Duration::DAY,
           credit_debit: :credit,
           order_instruction: :open,
+          quantity: quantity
+        )
+      end
+    end
+
+    context 'with a vertical roll order' do
+      let(:vertical_roll_options) do
+        {
+          strategy_type: SchwabRb::Order::ComplexOrderStrategyTypes::VERTICAL_ROLL,
+          account_number: account_number,
+          quantity: quantity,
+          price: price,
+          close_short_leg_symbol: 'SPX_CLOSE_SHORT',
+          close_long_leg_symbol: 'SPX_CLOSE_LONG',
+          open_short_leg_symbol: 'SPX_OPEN_SHORT',
+          open_long_leg_symbol: 'SPX_OPEN_LONG'
+        }
+      end
+
+      it 'delegates to VerticalRollOrder.build with explicit parameters' do
+        described_class.build(**vertical_roll_options)
+        expect(SchwabRb::Orders::VerticalRollOrder).to have_received(:build).with(
+          close_short_leg_symbol: 'SPX_CLOSE_SHORT',
+          close_long_leg_symbol: 'SPX_CLOSE_LONG',
+          open_short_leg_symbol: 'SPX_OPEN_SHORT',
+          open_long_leg_symbol: 'SPX_OPEN_LONG',
+          price: price,
+          stop_price: nil,
+          order_type: nil,
+          duration: SchwabRb::Orders::Duration::DAY,
+          credit_debit: :credit,
+          quantity: quantity
+        )
+      end
+
+      it 'handles debit rolls' do
+        debit_options = vertical_roll_options.merge(credit_debit: :debit)
+        described_class.build(**debit_options)
+        expect(SchwabRb::Orders::VerticalRollOrder).to have_received(:build).with(
+          close_short_leg_symbol: 'SPX_CLOSE_SHORT',
+          close_long_leg_symbol: 'SPX_CLOSE_LONG',
+          open_short_leg_symbol: 'SPX_OPEN_SHORT',
+          open_long_leg_symbol: 'SPX_OPEN_LONG',
+          price: price,
+          stop_price: nil,
+          order_type: nil,
+          duration: SchwabRb::Orders::Duration::DAY,
+          credit_debit: :debit,
+          quantity: quantity
+        )
+      end
+
+      it 'handles stop limit orders with stop_price' do
+        stop_price = 1.00
+        stop_limit_options = vertical_roll_options.merge(stop_price: stop_price)
+        described_class.build(**stop_limit_options)
+        expect(SchwabRb::Orders::VerticalRollOrder).to have_received(:build).with(
+          close_short_leg_symbol: 'SPX_CLOSE_SHORT',
+          close_long_leg_symbol: 'SPX_CLOSE_LONG',
+          open_short_leg_symbol: 'SPX_OPEN_SHORT',
+          open_long_leg_symbol: 'SPX_OPEN_LONG',
+          price: price,
+          stop_price: stop_price,
+          order_type: nil,
+          duration: SchwabRb::Orders::Duration::DAY,
+          credit_debit: :credit,
           quantity: quantity
         )
       end
